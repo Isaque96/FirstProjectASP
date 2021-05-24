3925260f7e22b114d8d02382928dd0b63b48c8c9
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-05-24 11:12:57 -0300

    Returning Custom Error Page

--- Project/Models/ViewModels/ErrorViewModel.cs
+++ Project/Models/ViewModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace Project.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
